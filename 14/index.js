3af658d9cb28d1a9ac5ca1725df68765c896c99f
Eitan Greene <none>
2021-10-22 13:41:00 -0700

    Refactor Euler 14 to not store all the CollatzNumbers

--- 14/index.js
+++ 14/index.js
@@ -1,27 +1,23 @@
 class CollatzNumber {
     constructor(n, cache) {
-        this.value = n
         this.length = 1
         if (n !== 1) {
             const next = (n % 2 === 0) ? (n / 2) : (3 * n + 1)
-            this.next = cache[next] || new CollatzNumber(next, cache)
-            this.length = this.next.length + 1
+            this.length = (cache[next] || new CollatzNumber(next, cache).length) + 1
         }
-        cache[n] = this
+        cache[n] = this.length
     }
 }
 
-// This should probably be improved to not require O(max) space
 class CollatzSequence {
     constructor(max) {
-        this.nodes = {}
+        this.cache = {}
         this.max = { number: 1, length: 1 }
         for (let i = 1; i <= max; i++) {
-            if (this.nodes[i]) { continue }
-            const node = new CollatzNumber(i, this.nodes)
+            if (this.cache[i]) { continue }
+            const node = new CollatzNumber(i, this.cache)
             if (node.length > this.max.length) {
-                this.max.number = i
-                this.max.length = node.length
+                this.max = { number: i, length: node.length }
             }
         }
     }
